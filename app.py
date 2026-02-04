32f5c597639e67f28bce97f3cb0af237bfe3644d
Tracy Vu <tracyvu@terpmail.umd.edu>
2026-02-03 21:20:00 -0500

    Added new route for hello HTML page.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Tracy Vu! This is my first HTML page.'
+    return 'Hello World from Tracy Vu! I am adding my first code change.'
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 
 
 if __name__ == '__main__':
