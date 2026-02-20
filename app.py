06bd19c76a2b0c3b1e88a338a6657d909c83d6c3
tracyvu <tracyvu@terpmail.umd.edu>
2026-02-20 18:08:56 -0500

    Changed app.py to load hello.html as a default path and added hyperlink to about.html

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Tracy Vu! I am adding my first code change.'
-
-@app.route('/hello')
-def hello():
     return render_template('hello.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
