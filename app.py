93dfd80fbe89cbd3ae66c380d77577c7a9b67675
tracyvu <tracyvu@terpmail.umd.edu>
2026-03-13 13:01:52 -0400

    Add favorite-course.html and contact.html with Bootstrap base template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,31 @@ def about():
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject')
+    course_number = request.args.get('course_number')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        major = request.form.get('major')
+
+        return render_template(
+            'contact.html',
+            submitted=True,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            major=major
+        )
+
+    return render_template('contact.html', submitted=False)
 
 if __name__ == '__main__':
     app.run()
